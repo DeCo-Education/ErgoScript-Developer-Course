d4aa1fc35e1443134d3cf7eac6a3856eee2d62d8
Keith Lim <keithlimweiluen@gmail.com>
2022-05-04 20:17:49 -0700

    added script

--- src/main/scala/DeCo/Playground/PlaygroundWithClient.scala
+++ src/main/scala/DeCo/Playground/PlaygroundWithClient.scala
@@ -10,6 +10,13 @@ object PlaygroundWithClient extends App {
     RestApiErgoClient.getDefaultExplorerUrl(NetworkType.MAINNET))
 
   val boxId = ""
+
+  val pinLockScript = s"""
+    sigmaProp(INPUTS(0).R4[Coll[Byte]].get == blake2b256(OUTPUTS(0).R4[Coll[Byte]].get))
+  """.stripMargin
+
+  val pinLockCode = ""
+
   client.execute(ctx => {
     val inputBox1 = ctx.getBoxesById(boxId).head
     println(inputBox1)
